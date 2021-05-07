23ff9d369bfe9a0dbf2c9bba85e2dbaae1cf1dcf
dostcarmis <dostcar.mis@gmail.com>
2021-05-07 09:01:16 +0800

    Rebuild release for v1.1.1

--- GSM Client/Properties/AssemblyInfo.cs
+++ GSM Client/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DRRMIS GSM Client")]
-[assembly: AssemblyDescription("SMS text blasting linked to DRRMIS. Arduino GSM module compatible and other microcontroller.")]
+[assembly: AssemblyDescription("Desktop application interface to DRRMIS (drrmis.dostcar.ph) GSM Module API.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Department of Science and Technology - CAR")]
 [assembly: AssemblyProduct("DRRMIS GSM Client")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
